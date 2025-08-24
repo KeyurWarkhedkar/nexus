90560a06f28c168084795cefcf4147e0b146d07d
KeyurWarkhedkar <keyurwarkhedkar@gmail.com>
2025-08-24 15:07:33 +0530

    Initial commit - Setup Spring Boot project (Nexus)

---
+++ src/test/java/com/keyur/nexus/NexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyur.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
